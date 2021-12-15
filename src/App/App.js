bb686def91111131225ae6734b4249e68e346270
Rainly0036 <icetornado36@gmail.com>
2021-12-15 20:39:17 +0800

    .

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from '../Pages/Home/Home.js'
 import SignUp from '../Pages/SignUp/SignUp.js'
 import Login from '../Pages/Login/Login.js'
@@ -7,19 +7,33 @@ import NavBar from '../Components/NavBar'
 import { useAuthContext } from '../Hooks/useAuthContext'
 
 function App() {
-    const { authIsReady } = useAuthContext()
+    const { authIsReady, user } = useAuthContext()
     return (
         <div className="App">
             {authIsReady && (
                 <BrowserRouter>
                 <NavBar />
                     <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/signup" element={<SignUp />} />
-                        <Route path="/login" element={<Login />} />
+
+                    {/* ROUTES 
+
+                        When user logged in, redirect to home page
+                        If not, redirect to login page
+
+                        When user logged out, redirect to login page
+                        If not, redirect to home page
+
+                        When user logged in and the sign up button is clicked, redirect to home page
+                        If not, redirect to sign up page 
+                        
+                    */}
+
+                        <Route path="/" element={!user ? <Navigate replace to="/login" /> : <Home />} />
+                        <Route path="/signup" element={user ? <Navigate replace to="/" /> : <SignUp />} />
+                        <Route path="/login" element={user ? <Navigate replace to="/" /> : <Login />} />
                     </Routes>
                 </BrowserRouter>
-            )}
+            )}                      
         </div>
     )
 }
